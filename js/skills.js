069e87092e11ccb20fff00b529c941d6c8051160
Birendra848 <75802334+Birendra848@users.noreply.github.com>
2020-12-18 23:27:57 +0530

    Add files via upload

---
+++ js/skills.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
